020f6842eef970099095dad21962bea9686c66a4
GiyeonChoi <bdcgy22@daum.net>
2025-01-31 20:50:49 +0900

    first commit

---
+++ src/test/java/com/giyeon/githubactionspractice/GithubActionsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giyeon.githubactionspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
