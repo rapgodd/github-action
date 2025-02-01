136ca58cf641770b2e00df89e62da3688c0b4332
GiyeonChoi <bdcgy22@daum.net>
2025-02-01 20:40:25 +0900

    docker + githubaction + ecr을 이용한 가장 간단한 ci/cd파이프라인 구축후 테스트

--- src/test/java/com/giyeon/githubactionspractice/GithubActionsPracticeApplicationTests.java
+++ src/test/java/com/giyeon/githubactionspractice/GithubActionsPracticeApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class GithubActionsPracticeApplicationTests {
 
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+
+    }
 
 }
